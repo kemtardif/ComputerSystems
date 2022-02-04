ea8bd4d346dc4897598eaf2a41da2c573ba5a3c9
Kem Tardif <tardif.kremlin@gmail.com>
2022-02-04 06:24:42 -0500

    Lab

--- Labs/cachelab-handout/csim.c
+++ Labs/cachelab-handout/csim.c
@@ -4,25 +4,64 @@
 #include <getopt.h>
 #include <string.h>
 
-void parseCLArgs(long * sp, long * Ep, long * bp, FILE * fp, int argc, char *argv[]);
+struct cacheLine 
+{
+    unsigned int valid;
+    unsigned long tag;
+};
+
+
+void parseArgs(long * sp, long * Ep, long * bp, FILE ** fp, int argc, char *argv[]);
+void InitCache(struct cacheLine **cache, long S, long E);
+void freeMemory(struct cacheLine **cache, long S);
 
 int main(int argc, char *argv[])
 {
-    //Define our cache
-    long s; 
+    //Cache parameters
+    struct cacheLine **cache;
+    long s;
+    long S;
+    //long t; 
     long E;
-    long b; 
+    long b;
+
+    //File reading parameters
     FILE *f = NULL;
+    char fline[1000];
+    // size_t flen = 0;
+    // size_t fread;
 
     //First parse the command-line arguments
-    parseCLArgs(&s, &E, &b, f, argc, argv);
+    parseArgs(&s, &E, &b, &f, argc, argv);
+    S = 1 << s;
+    //t = (1 << 6) - s - b;
 
+    //Initialize cache with given values
+    cache = malloc(S * sizeof(struct cacheLine *));
+    if(cache)
+    {
+        InitCache(cache, S, E);
+    }
+    else 
+    {
+        fprintf(stderr, "Error while creating the cache.");
+        exit(EXIT_FAILURE);
+    }
+
+    //Read file
+    while (fgets(fline,100, f)) {
+        //Check the instruction
+        //Read or write to te cache
+        //update counter
+    }
 
     //printSummary(0, 0, 0);
+    fclose(f);
+    freeMemory(cache, S);
     return 0;
 }
 
-void parseCLArgs(long *sp, long *Ep, long *bp, FILE *fp, int argc, char *argv[])
+void parseArgs(long *sp, long *Ep, long *bp, FILE **fp, int argc, char *argv[])
 {
     long c;
     opterr = 0;
@@ -40,8 +79,8 @@ void parseCLArgs(long *sp, long *Ep, long *bp, FILE *fp, int argc, char *argv[])
             *bp = atol(optarg);  
             break;
             case 't':
-            fp = fopen(optarg, "r");  
-            if(fp == NULL)
+            *fp = fopen(optarg, "r");  
+            if(!*fp)
             {
                 fprintf(stderr, "%s : Not a valid file path\n",optarg);
                 exit(EXIT_FAILURE);
@@ -52,4 +91,36 @@ void parseCLArgs(long *sp, long *Ep, long *bp, FILE *fp, int argc, char *argv[])
             exit(EXIT_FAILURE);
         }
     }
+}
+void InitCache(struct cacheLine **cache, long S, long E)
+{
+    long i;
+    long y;
+    
+    for(i = 0; i < S; i++)
+    {      
+        *(cache + i) = malloc(E * sizeof(struct cacheLine));
+        if(*(cache + i))
+        {
+            for(y = 0; y < E; y++)
+            {
+                ((*(cache + i)) + y)->valid = 0;
+            }
+        }
+        else
+        {
+            fprintf(stderr, "Error while creating the cache.");
+            exit(EXIT_FAILURE);
+        }
+    }
+}
+
+void freeMemory(struct cacheLine **cache, long S)
+{
+    int i;
+    for(i = 0; i < S; i++)
+    {      
+        free(*(cache + i));
+    }
+    free(cache);
 }
